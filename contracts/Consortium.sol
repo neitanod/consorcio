c293c362726bb0287e9f9bc8346fcf436a7fe4c9
ggrignoli <gerardo.grignoli@vmbc.com>
2017-11-29 00:06:07 -0300

    Consortuim contract initialization

--- contracts/Consortium.sol
+++ contracts/Consortium.sol
@@ -1,9 +1,54 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.18;
 
 contract Consortium {
-    uint a = 15;
-
-    function doNothing() public {
-        a++;
+    
+    constant uint TOTAL = 10000;
+    
+    uint[] holder_participation;
+    string[] holder_name;
+    address[] holder_account;
+    bool initialized=false;
+    
+    uint requiredVotes;
+    uint holderCount=0;
+    uint totalParticipation=0;
+    
+    modifier onlyDuringInitialization {
+        require(initialized==false);
+        _;
     }
+    
+    function Consortium(uint requiredVote) public {
+        requiredVotes = requiredVote;
+        initialized=false;
+    }
+    
+    function AddHolder(string name, address account, uint participation)  
+        public onlyDuringInitialization  {
+        
+        require(participation>0);
+        require(totalParticipation+participation<=TOTAL);
+        
+        totalParticipation+=participation;
+        holder_participation.push(participation);
+        holder_name.push(name);
+        holder_account.push(account);
+        
+        holderCount++;
+        
+        if(totalParticipation==TOTAL)
+            initialized=true;
+    }
+        
+    function IsInitialized() public constant returns (bool) {
+        return initialized;
+    }
+    
+    function getHolder(uint i) public constant returns (string name, address account, uint participation) {
+        require(i>=0);
+        require(i<holderCount);
+        
+        return (holder_name[i], holder_account[i], holder_participation[i]);
+    }
+    
 }

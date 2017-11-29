cbea00ca2d4620dc4c8add8c18a60988794d2bf2
ggrignoli <gerardo.grignoli@vmbc.com>
2017-11-28 22:40:22 -0300

    Added empty contracts and migrations

---
+++ contracts/Consortium.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.4;
+
+contract Consortium {
+    uint a = 15;
+
+    function doNothing() public {
+        a++;
+    }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var Consortium = artifacts.require("Consortium");
+
+module.exports = function(deployer) {
+  deployer.deploy(Consortium);
+};
